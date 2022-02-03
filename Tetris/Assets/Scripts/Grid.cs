df3d3e8c8ca87b18873a8d241ae92548220ce505
davidKael <70914882+davidKael@users.noreply.github.com>
2022-02-03 17:51:22 +0100

    rearrangingPart2
    might be new errors somewhere

---
+++ Tetris/Assets/Scripts/Grid.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    internal Dictionary<Vector2Int, Box> Boxes = new Dictionary<Vector2Int, Box>();
+
+    private void Start()
+    {
+        SetUpBoxes();
+    }
+
+    void SetUpBoxes()
+    {
+        foreach(Box box in transform.GetComponentsInChildren<Box>())
+        {
+            Boxes.Add(box.pos, box);
+            box.grid = this;
+        }
+    }
+
+
+    internal void MoveDownAll(int lowestRow, int amount)
+    {
+        for (int row = lowestRow; row < 20; row++)
+        {
+            for (int col = 0; col < 10; col++)
+            {
+                // Debug.Log($"row={row},col={col}");
+                Boxes[new Vector2Int(col, row)].MoveDown(amount);
+            }
+        }
+    }
+
+    internal void DeleteFormFromGrid(BoxFormation boxFormation)
+    {
+        foreach (Vector2Int pos in boxFormation.boxPositions)
+        {
+            Boxes[pos].ResetBox();
+
+        }
+        foreach (Vector2Int ghostPos in boxFormation.ghosts)
+        {
+            Boxes[ghostPos].ResetBox();
+        }
+    }
+
+
+}
